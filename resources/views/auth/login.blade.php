294355794626734b011b1bbf0b7ba3562b715e6f
Dajken <dawidsob7753@wp.pl>
2024-05-14 15:02:58 +0100

    added form style

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
-@section('title', 'Register')
+@section('title', 'Login')
 @section('content')
     <div class="row justify-content-center mt-5">
-        <div class="col-md-8">
+        <div class="col-md-4">
     
             <div class="card">
                 <div class="card-header">Login</div>
