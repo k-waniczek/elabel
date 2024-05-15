6976735fab517f0b85ba6c91b71172c8ee3d2ea7
Oskar Sroka <oskar.sroka.zeg@gmail.com>
2024-05-15 16:44:14 +0200

    adjusted styles and layout of form

--- resources/views/wines/create.blade.php
+++ resources/views/wines/create.blade.php
@@ -24,8 +24,8 @@
                             <label for="vintage" class="form-label">Vintage</label>
                             <input type="number" class="form-control" id="vintage" name="vintage">
                         </div>
-                    <div class="flex-box">
-                        <div class="mb-3 w-25">
+                    <div class="row mb-3">
+                        <div class="col-sm">
                             <label for="type" class="form-label">Type</label>
                             <select class="form-select" id="type" name="type">
                                 @foreach (config('app.wine_types') as $type)
@@ -33,7 +33,7 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="mb-3 w-25">
+                        <div class="col-sm">
                             <label for="style" class="form-label">Style</label>
                             <select class="form-select" id="style" name="style">
                                 @foreach (config('app.wine_styles') as $style)
@@ -41,7 +41,7 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="mb-3 w-25">
+                        <div class="col-sm">
                             <label for="sugar_content" class="form-label">Sugar content</label>
                             <select class="form-select" id="sugar_content" name="sugar_content">
                                 @foreach (config('app.wine_sugar_contents') as $sc)
@@ -49,14 +49,14 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="mb-3 w-25">
+                        </div>
+                        <div class="mb-3 w-100">
                             <label for="packaging_gases" class="form-label">Packaging gases</label>
                             <select class="form-select" id="packaging_gases" name="packaging_gases">
                                 @foreach (config('app.gases') as $pg)
                                     <option value="{{$pg}}">{{$pg}}</option>
                                 @endforeach
                             </select>
-                        </div>
                         </div>
                         <div class="mb-3">
                             <label for="appellation" class="form-label">Appellation</label>
@@ -102,54 +102,55 @@
                             <label for="salt" class="form-label">Salt</label>
                             <input type="number" class="form-control" id="salt" name="salt">
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label">Warnings:</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox"
-                                    id="warning_drinking_during_pregnancy" name="warning_drinking_during_pregnancy">
-                                <label class="form-check-label" for="warning_drinking_during_pregnancy">
-                                    Drinking during pregnancy
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox"
-                                    id="warning_drinking_below_legal_age" name="warning_drinking_below_legal_age">
-                                <label class="form-check-label" for="warning_drinking_below_legal_age">
-                                    Drinking below legal age
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox"
-                                    id="warning_drinking_when_driving" name="warning_drinking_when_driving">
-                                <label class="form-check-label" for="warning_drinking_when_driving">
-                                    Drinking when driving
-                                </label>
+                        <div class="row mb-3">
+
+                            <div class="col-sm">
+                                <label class="form-label">Warnings:</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox"
+                                        id="warning_drinking_during_pregnancy" name="warning_drinking_during_pregnancy">
+                                    <label class="form-check-label" for="warning_drinking_during_pregnancy">
+                                        Drinking during pregnancy
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox"
+                                        id="warning_drinking_below_legal_age" name="warning_drinking_below_legal_age">
+                                    <label class="form-check-label" for="warning_drinking_below_legal_age">
+                                        Drinking below legal age
+                                    </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox"
+                                        id="warning_drinking_when_driving" name="warning_drinking_when_driving">
+                                    <label class="form-check-label" for="warning_drinking_when_driving">
+                                        Drinking when driving
+                                    </label>
+                                </div>
+                        </div>
+
+                            <div class="col-sm" id="cert-column">
+                                <label class="form-label">Certifications:</label>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" value="1" id="certifications_organic"
+                                        name="certifications_organic">
+                                    <label class="form-check-label" for="certifications_organic">
+                                        Organic
+                                    </label>
+                                    <input class="form-check-input" type="checkbox" value="1" id="certifications_vegetarian"
+                                        name="certifications_vegetarian">
+                                    <label class="form-check-label" for="certifications_vegetarian">
+                                        Vegetarian
+                                    </label>
+                                    <input class="form-check-input" type="checkbox" value="1" id="certifications_vegan"
+                                        name="certifications_vegan">
+                                    <label class="form-check-label" for="certifications_vegan">
+                                        Vegan
+                                    </label>
+                                </div>
                             </div>
                         </div>
-                        <div class="mb-3">
-                            <label class="form-label">Certifications:</label>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="1" id="certifications_organic"
-                                    name="certifications_organic">
-                                <label class="form-check-label" for="certifications_organic">
-                                    Organic
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="1" id="certifications_vegetarian"
-                                    name="certifications_vegetarian">
-                                <label class="form-check-label" for="certifications_vegetarian">
-                                    Vegetarian
-                                </label>
-                            </div>
-                            <div class="form-check">
-                                <input class="form-check-input" type="checkbox" value="1" id="certifications_vegan"
-                                    name="certifications_vegan">
-                                <label class="form-check-label" for="certifications_vegan">
-                                    Vegan
-                                </label>
-                            </div>
-                        </div>
+
                         <div class="mb-3">
                             <label for="country" class="form-label">Country</label>
                             <input type="text" class="form-control" id="country" name="country">
